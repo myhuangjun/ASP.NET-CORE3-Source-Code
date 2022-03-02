d4600db4c2b3feffda356a7ad3e8c5efb9f98d4d
851248673@qq.com <851248673@qq.com>
2022-03-02 21:39:56 +0800

    更新至1.1.3

---
+++ 1.1.3.helloword/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Http;
+using System;
+
+namespace _1._1._3.helloword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //第一种
+            //Host.CreateDefaultBuilder().ConfigureWebHost(
+            //    webHostBuild => webHostBuild.
+            //    UseKestrel().
+            //    //UseUrls("http://localhost:8000","https://localhost:8001").显示指定url
+            //    Configure(app => app.Run(
+            //            context => context.Response.WriteAsync("Hello Word")
+            //        )
+            //    )
+            // ).Build().Run();
+            //ConfigureWebHostDefaults方式
+            Host.CreateDefaultBuilder().ConfigureWebHostDefaults(webHostBuild =>
+            {
+                webHostBuild.Configure(app => app.Run(context => context.Response.WriteAsync("Hello Word")));
+            }).Build().Run();
+        }
+    }
+}
